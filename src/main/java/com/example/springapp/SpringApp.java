773803b4b9581f0e0e4e2653abb00cd18c81e714
vmoiseeva <vmoiseeva@diasoft.ru>
2025-10-01 19:35:57 +0300

    Task 5

--- src/main/java/com/example/springapp/SpringApp.java
+++ src/main/java/com/example/springapp/SpringApp.java
@@ -1,27 +1,24 @@
 package com.example.springapp;
 
-import com.example.springapp.service.ExamService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.MessageSource;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.context.event.ApplicationEventMulticaster;
+import org.springframework.context.event.SimpleApplicationEventMulticaster;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
 
 import java.util.Scanner;
 
 @SpringBootApplication
-@ComponentScan("com.example.springapp.config")
 public class SpringApp {
-    private static final Scanner SC = new Scanner(System.in);
+
+    public ApplicationEventMulticaster applicationEventMulticaster() {
+        SimpleApplicationEventMulticaster eventMulticaster = new SimpleApplicationEventMulticaster();
+        eventMulticaster.setTaskExecutor(new SimpleAsyncTaskExecutor());
+        return eventMulticaster;
+    }
 
     public static void main(String[] args) {
-        ApplicationContext context =
-                SpringApplication.run(SpringApp.class, args);
 
-        ExamService examService = context.getBean(ExamService.class);
-        ClassPathResource csvResource = context.getBean(ClassPathResource.class);
-        MessageSource msg = context.getBean(MessageSource.class);
-        examService.print(csvResource, SC, msg);
+        SpringApplication.run(SpringApp.class, args);
     }
 }
