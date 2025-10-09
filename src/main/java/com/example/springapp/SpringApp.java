3292af51d4e96d5822447f78185fbc9a5eb376f4
vmoiseeva <vmoiseeva@diasoft.ru>
2025-10-09 19:01:06 +0300

    Task 4 - Resolved issues

--- src/main/java/com/example/springapp/SpringApp.java
+++ src/main/java/com/example/springapp/SpringApp.java
@@ -4,24 +4,17 @@ import com.example.springapp.service.ExamService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.core.io.ClassPathResource;
-
-import java.util.Scanner;
 
 @SpringBootApplication
 @ComponentScan("com.example.springapp.config")
 public class SpringApp {
-    private static final Scanner SC = new Scanner(System.in);
 
     public static void main(String[] args) {
         ApplicationContext context =
                 SpringApplication.run(SpringApp.class, args);
 
         ExamService examService = context.getBean(ExamService.class);
-        ClassPathResource csvResource = context.getBean(ClassPathResource.class);
-        MessageSource msg = context.getBean(MessageSource.class);
-        examService.print(csvResource, SC, msg);
+        examService.print();
     }
 }
